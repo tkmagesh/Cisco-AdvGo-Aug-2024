e4e07eaf4cd4458cf4df83068ba3f6ad480b390a
Magesh Kuppan <tkmagesh77@gmail.com>
2024-08-28 14:43:35 +0530

    01-assignment - solution

--- 02-concurrency/00-assignments/01-assignment.go
+++ 02-concurrency/00-assignments/01-assignment.go
@@ -16,3 +16,48 @@ main()
 
 DO NOT create a waitgroup at package scope
 */
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go PrintNos(wg)
+	wg.Wait()
+	fmt.Println("Done")
+}
+
+func PrintNos(wg *sync.WaitGroup) {
+	defer wg.Done()
+	wg1 := &sync.WaitGroup{}
+	wg1.Add(1)
+	go PrintEvenNos(wg1)
+
+	wg1.Add(1)
+	go PrintOddNos(wg1)
+	wg1.Wait()
+}
+
+func PrintEvenNos(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i, count := 0, 20; count > 0; count-- {
+		i += 2
+		fmt.Println("Even No :", i)
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func PrintOddNos(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i, count := 1, 20; count > 0; count-- {
+		i += 2
+		fmt.Println("Odd No :", i)
+		time.Sleep(2 * time.Second)
+	}
+}
