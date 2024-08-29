c216dcab612f2fc983348e5777b857dbb1fd9319
Magesh Kuppan <tkmagesh77@gmail.com>
2024-08-29 09:55:51 +0530

    assignment-04 problem (workers)

---
+++ 02-concurrency/00-assignments/04-assignment.go
@@ -0,0 +1,46 @@
+/*
+modify the program spin up finite # of workers (5 or 10 or 15)
+the workers have to share the load of processing the range of numbers
+*/
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	primesCh := findPrimes(1000, 20000)
+	for no := range primesCh {
+		fmt.Println("Prime :", no)
+	}
+	fmt.Println("Done")
+}
+
+func findPrimes(start, end int) <-chan int {
+	primesCh := make(chan int)
+	go func() {
+		wg := &sync.WaitGroup{}
+		for no := start; no <= end; no++ {
+			wg.Add(1)
+			go func() { //worker
+				defer wg.Done()
+				if isPrime(no) {
+					primesCh <- no
+				}
+			}()
+		}
+		wg.Wait()
+		close(primesCh)
+	}()
+	return primesCh
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
